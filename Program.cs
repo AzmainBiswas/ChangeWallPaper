5f8447d09d9b4e1798ef62b4839ac488ce495e26
Azmain Biswas <azmainbiswas007@gmail.com>
2024-02-01 21:07:29 +0530

    WallPaper Change

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ChangeWallPaper
+{
+    // StackOverFlow Shit
+    class Magician
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int HIDE = 0;
+        const int SHOW = 5;
+
+        public static void DisappearConsole()
+        {
+            ShowWindow(GetConsoleWindow(), HIDE);
+        }
+    }
+
+    class WallPaper
+    {
+        // Global Constant Variable for changing the wallpaper
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 1;
+        const int SPIF_SENDCHANGE = 2;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool SystemParametersInfo(int uiAction, int uiParam, string pvParam, int fWinIni);
+
+        static void Main()
+        {
+            // wallpaper directory
+            string wallPaperPath = @"E:\wallpapers";
+
+            if (Directory.Exists(wallPaperPath))
+            {
+                // Console.WriteLine("Changing WallPaper!!!!");
+                Magician.DisappearConsole();
+                // a array of name of all wallpapers
+                string[] allWallPaper = Directory.GetFiles(wallPaperPath);
+                // generate a random integer
+                Random random = new Random();
+                int index = random.Next(0, allWallPaper.Length);
+                // random Wallpaper
+                string wallPaper = allWallPaper[index];
+                // set wallpaper
+                SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, wallPaper, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+                // Console.WriteLine("Wallpaper Changed to " + wallPaper);
+            }
+        }
+    }
+}
